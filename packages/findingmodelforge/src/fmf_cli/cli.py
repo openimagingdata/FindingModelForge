ff7aa9c7b13622659f71eda00fe9475389c2c582
Tarik Alkasab <tarik@alkasab.org>
2024-11-25 16:59:46 -0500

    feat(findingmodelforge.fmf_cli): Using rich to show status spinners during long-running tasks

--- packages/findingmodelforge/src/fmf_cli/cli.py
+++ packages/findingmodelforge/src/fmf_cli/cli.py
@@ -1,6 +1,5 @@
 import asyncio
 from pathlib import Path
-from pprint import pprint
 
 import click
 from findingmodelforge.config import (  # type: ignore
@@ -12,6 +11,7 @@ from findingmodelforge.config import (  # type: ignore
 from findingmodelforge.finding_info_tools import describe_finding_name, get_detail_on_finding
 from findingmodelforge.finding_model_tools import create_finding_model_from_markdown
 from findingmodelforge.models.finding_info import BaseFindingInfo
+from rich.console import Console
 
 
 def get_settings():
@@ -19,6 +19,8 @@ def get_settings():
         "DEBUG": settings.DEBUG,
         "MONGO_DSN": settings.MONGO_DSN,
         "DATABASE_NAME": settings.DATABASE_NAME,
+        "DEFAULT_OPENAI_MODEL": settings.DEFAULT_OPENAI_MODEL,
+        "DEFAULT_PERPLEXITY_MODEL": settings.DEFAULT_PERPLEXITY_MODEL,
     }
 
     def format_secret(secret: str) -> str:
@@ -39,8 +41,9 @@ def cli():
 @cli.command()
 def config():
     """Show the currently active configuration."""
-    print("Finding Model Forge configuration:")
-    pprint(get_settings())
+    console = Console()
+    console.print("[yellow bold]Finding Model Forge configuration:")
+    console.print(get_settings())
 
 
 @cli.command()
@@ -48,27 +51,38 @@ def config():
 def make_info(finding_name):
     """Generate description/synonyms and more details/citations for a finding name."""
     if not check_ready_for_openai():
+        raise ConfigurationError("OpenAI API key not set (use OPENAI_API_KEY in .env).")
+
+    console = Console()
+    console.print(f"[gray] Getting information on [yellow bold]{finding_name}")
+    with console.status("[bold green]Getting description and synonyms..."):
         described_finding = asyncio.run(describe_finding_name(finding_name))
-        pprint(described_finding)
-        if check_ready_for_perplexity():
+    if check_ready_for_perplexity():
+        with console.status("Getting detailed information... "):
             detailed_response = asyncio.run(get_detail_on_finding(described_finding))
-            pprint(detailed_response)
+        console.print(detailed_response)
+    else:
+        console.print(described_finding)
 
 
 @cli.command()
 # Indicate that the argument should be a filename
-@click.argument("finding_path", type=click.Path(exists=True))
+@click.argument("finding_path", type=click.Path(exists=True, path_type=Path))
 def markdown_to_fm(finding_path: Path):
     """Convert markdown file to finding model format."""
-    finding_path = Path(finding_path)
-    print(f"Converting {finding_path} to finding model format.")
-    # Get the part of the path that is the finding name (no directory, no extension)
-    finding_name = finding_path.stem.replace("_", " ").replace("-", " ")
+
     if not check_ready_for_openai():
         raise ConfigurationError("OpenAI API key not set (use OPENAI_API_KEY in .env).")
-    described_finding = asyncio.run(describe_finding_name(finding_name))
+
+    console = Console()
+    finding_name = finding_path.stem.replace("_", " ").replace("-", " ")
+    console.print(f"[gray] Getting model for [yellow bold]{finding_name}")
+    click.echo(f"Converting {finding_path} to finding model format.")
+    with console.status("[bold green]Getting description..."):
+        described_finding = asyncio.run(describe_finding_name(finding_name))
+        console.print(described_finding)
     if not isinstance(described_finding, BaseFindingInfo):
         raise ValueError("Finding info not returned.")
-    pprint(described_finding)
-    model = asyncio.run(create_finding_model_from_markdown(described_finding, markdown_path=finding_path))
-    pprint(model)
+    with console.status("Creating model from Markdown description..."):
+        model = asyncio.run(create_finding_model_from_markdown(described_finding, markdown_path=finding_path))
+    console.print(model)
