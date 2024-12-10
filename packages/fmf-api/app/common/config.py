2e8f6e87f9587e629373819cdd1987fa53b87ee2
Tarik Alkasab <tarik@alkasab.org>
2024-12-10 16:39:58 -0500

    fix(findingmodelforge.config): Added quote-stripping, including of SecretStrs

--- packages/fmf-api/app/common/config.py
+++ packages/fmf-api/app/common/config.py
@@ -1,7 +1,7 @@
 import secrets
 
-from findingmodelforge.config import FindingModelForgeConfig
-from pydantic import Field, SecretStr
+from findingmodelforge.config import FindingModelForgeConfig, QuoteStrippedSecretStr, QuoteStrippedStr
+from pydantic import Field
 from typing_extensions import Final
 
 GITHUB_AUTHORIZE_URL: Final[str] = "https://github.com/login/oauth/authorize"
@@ -19,13 +19,15 @@ class FindingModelForgeAPIConfig(FindingModelForgeConfig):
     github_access_token_url: str = Field(default=GITHUB_ACCESS_TOKEN_URL)
     github_user_info_url: str = Field(default=GITHUB_USER_INFO_URL)
 
-    github_client_id: str
-    github_client_secret: SecretStr
+    github_client_id: QuoteStrippedStr
+    github_client_secret: QuoteStrippedSecretStr
 
     login_path: str = Field(default=LOGIN_PATH)
     auth_redirect_uri: str = Field(default=AUTH_REDIRECT_URI)
 
-    storage_secret: SecretStr = Field(default_factory=lambda: SecretStr(secrets.token_hex(16)))
+    storage_secret: QuoteStrippedSecretStr = Field(
+        default_factory=lambda: QuoteStrippedSecretStr(secrets.token_hex(16))
+    )
 
     enable_ssl: bool = Field(default=False)
     ssl_certfile: str | None = None
--- packages/fmf-api/run_app_dev.sh
+++ packages/fmf-api/run_app_dev.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Fail unless we have the required environment variables
-if [ -z "$GITHUB_CLIENT_ID" ] || [ -z "$GITHUB_CLIENT_SECRET" ]; then
-    echo "Please set valid environment variables"
-    exit 1
-fi
 # use path of this example as working directory; enables starting this script from anywhere
 APP_DIR=/app
 cd $APP_DIR
@@ -13,4 +8,4 @@ cd $APP_DIR
 PORT=${FMF_API_PORT:-8000}
 echo "Starting Uvicorn server in dev mode..."
 # we also use a single worker in production mode so socket.io connections are always handled by the same worker
-exec uv run uvicorn app.main:app --reload --log-level info  --host localhost --port $PORT
+exec uv run uvicorn app.main:app --workers 1 --log-level info  --host 0.0.0.0 --port $PORT
