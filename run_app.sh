65d844c8e0d4ca2c4df75d3d514fb6b4e0f4db8c
Tarik Alkasab <tarik@alkasab.org>
2025-04-23 09:17:41 -0400

    fix(run_app.sh): Update shebang and improve environment variable defaults for server startup

--- run_app.sh
+++ run_app.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # use path of this example as working directory; enables starting this script from anywhere
 APP_DIR=/app
 cd $APP_DIR
 
-# Use PORT for PORT if it's set, otherwise default to 8000
+# set default values for environment variables if not already set
 PORT=${PORT:-8000}
-echo "Starting Uvicorn server on ${PORT}..."
+LOG_LEVEL=${LOG_LEVEL:-info}
+WORKERS=${WORKERS:-1}
+echo "Starting Uvicorn server on ${PORT}... ($WORKERS workers, ${LOG_LEVEL} log level)"
 # we also use a single worker in production mode so socket.io connections are always handled by the same worker
-exec uv run uvicorn findingmodelforge.main:app --workers 1 --log-level info  --host 0.0.0.0 --port $PORT
+exec uv run uvicorn findingmodelforge.main:app --workers $WORKERS --log-level $LOG_LEVEL --host 0.0.0.0 --port $PORT
